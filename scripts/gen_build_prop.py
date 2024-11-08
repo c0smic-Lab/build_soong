697a2015b2ec29f75b9c83305b142d6def491151
COSMIC <rayavanindra@gmail.com>
2024-11-08 22:48:58 +0530

    gen_build_prop: I only want to see build id

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -219,7 +219,7 @@ def generate_build_info(args):
       print(f"ro.build.display.id?={config['BuildId']} {config['BuildKeys']}")
   else:
     # Non-user builds should show detailed build information (See build desc above)
-    print(f"ro.build.display.id?={config['BuildDesc']}")
+    print(f"ro.build.display.id?={config['BuildId']}")
   print(f"ro.build.version.incremental={config['BuildNumber']}")
   print(f"ro.build.version.sdk={config['Platform_sdk_version']}")
   print(f"ro.build.version.sdk_minor={build_flags['RELEASE_PLATFORM_SDK_MINOR_VERSION']}")
